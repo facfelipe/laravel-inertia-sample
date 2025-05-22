8a3b17a56bc13d67e1dd91078aaa489fb8034c24
Felipe Carretta <fac.felipe8@gmail.com>
2025-05-21 20:39:16 -0400

    chore: initial project setup with Laravel, Inertia, Vite and Tailwind

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,11 @@
+<template>
+
+    <div>
+        <h1 class="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+        <p class="text-lg">Welcome to your new Laravel application.</p>
+        <p class="text-lg">This is a simple page created using Vue.js and Tailwind CSS.</p>
+    </div>
+
+</template>
